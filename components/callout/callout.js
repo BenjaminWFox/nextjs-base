b22ab970284c77d8525230f53f2f33b69217e97b
Ben Fox <ben.fox@lumedic.io>
2022-03-10 13:17:44 -0800

    Dont change package name. Remove old callout component

--- components/callout/callout.js
+++
@@ -1,9 +0,0 @@
-import PropTypes from 'prop-types'
-
-export default function Callout({ children }) {
-  return <p><strong style={{ color: 'red' }}>!</strong> {children} <strong style={{ color: 'red' }}>!</strong></p>
-}
-
-Callout.propTypes = {
-  children: PropTypes.node.isRequired
-}
